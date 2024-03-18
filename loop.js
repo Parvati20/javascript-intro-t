01da4ef05462df603b3628cec6f32caf9c0ea12a
Parvati20 <parvati23@nvgurukul.org>
2024-03-18 11:02:35 +0530

    first

---
+++ loop.js
@@ -0,0 +1,65 @@
+// for loop, while, do while
+
+//Entry controlled loop:  the condition is checked and then the operation is performed. FOR AND WHILE LOOP
+// for loop
+
+//For Loop
+// for(let i=0; i<=a; i++){
+//     console.log("hello")
+//  alert("This is the value of i");
+// }
+
+//While Loop
+
+// let i = 1;
+// while (i <= a) {
+//   console.log("This is a while loop");
+//   i++;
+// }
+
+//Do While Loop
+//do{
+
+//}while(condition);
+//EXIT Control Loop :The operation is done and then the condition is checked. Do while loop
+
+// let a = 3;
+// let i = 45;
+
+// do{
+//     console.log("This is a do while loop the value is", i);
+//     i++;
+// }while(i<=a);
+
+
+
+//FOR of loop: It loops through the values of the string or array(iterable object=> Cheez, thing).
+const arr = [1,2,3,54,"string"];
+// console.log(arr[3]);
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]+1);
+// }
+
+// for (let i of arr) {
+//   console.log(i+1);
+// }
+
+
+
+//OBJECTS ARE NOT ITERABLE IN JS
+
+//For In loops in JS: It loops through the properties of an object.
+
+// let obj = {
+//   name: "Ankush",
+//   age: 40,
+//   isMarried: true,
+//   role: "Manager",
+// };
+
+// for (let key in obj) {
+//   console.log(key,obj[key]);
+// }
+
+
+//FOR EACH LOOP :  For arrays only
